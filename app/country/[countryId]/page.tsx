49f3f95b55e6811f2bfa92f41fcac6a3a4f3187c
Yazdun <yazduntube@gmail.com>
2023-11-15 02:41:34 +0330

    fix: fetch data with rsc

--- app/country/[countryId]/page.tsx
+++ app/country/[countryId]/page.tsx
@@ -1,48 +1,15 @@
-'use client'
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { Country } from "../../../interfaces";
+import countriesServices from "../../../services/countries";
 
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import { Country } from '../../../interfaces'
-import countriesServices from '../../../services/countries'
-
-const CountryPage = () => {
-  const router = useRouter()
-  const { country } = router.query
-  const [countryData, setCountryData] = useState<Country | null>(null)
-  const [error, setError] = useState<string | null>(null)
-
-  useEffect(() => {
-    const getCountryData = async () => {
-      try {
-        if (typeof country === 'string') {
-          const data = await countriesServices.fetchCountry(country)
-          console.log(data)
-          setCountryData(data)
-        }
-      } catch (error: any) {
-        setError(error.message || 'An error occurred')
-      }
-    }
-
-    if (country) {
-      getCountryData()
-    }
-  }, [country])
-
-  if (error) {
-    return <div>Error: {error}</div>
-  }
-
-  if (!countryData) {
-    return <div>Loading...</div>
-  }
+export default async function ({ params }: { params: { countryId: string } }) {
+  const data = await countriesServices.fetchCountry(params.countryId);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       {/* Your content here using countryData */}
-      <h1>Hello</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </main>
-  )
+  );
 }
-
-export default CountryPage
