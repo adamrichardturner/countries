bd9acbd613dcd4c7b613e47eb321e5b08bd9f3b7
adamrichardturner <hello@adamrichardturner.dev>
2023-11-18 03:06:25 +0000

    Fixed typescript issues

--- services/countries.tsx
+++ services/countries.tsx
@@ -3,82 +3,106 @@ import {
   FetchCountriesParams,
   FetchCountriesByNameParams,
   FetchCountryParams,
-  FetchByRegionParams
+  FetchByRegionParams,
+  CountryName
 } from '@/interfaces'
 
 const baseUrl = `https://restcountries.com/v3.1/`
 const query = `?fields=name,flags,nativeName,population,region,subRegion,capital,tld,currencies,languages,borders,borderCountries,cca3`
 
-async function fetchAllCountries() {
-  const response = await fetch(`${baseUrl}all${query}`)
-  const data = (await response.json()) as Country[]
-  return { data }
+async function fetchAllCountries(): Promise<{ data: Country[] }> {
+  try {
+    const response = await fetch(`${baseUrl}all${query}`)
+    const data = (await response.json()) as Country[]
+    return { data }
+  } catch (error) {
+    console.error('Error fetching all countries', error)
+    throw error
+  }
 }
 
-async function fetchCountries({ params }: { params: FetchCountriesParams }) {
+async function fetchCountries(
+  params: FetchCountriesParams
+): Promise<{ data: Country[] }> {
   const page = params.page || 1
   const pageSize = params.pageSize
-  const response = await fetch(
-    `${baseUrl}all?limit=${pageSize}&start=${(page - 1) * pageSize}${query}`
-  )
-  const data = (await response.json()) as Country[]
-  return { data }
+  try {
+    const response = await fetch(
+      `${baseUrl}all?limit=${pageSize}&start=${(page - 1) * pageSize}${query}`
+    )
+    const data = (await response.json()) as Country[]
+    return { data }
+  } catch (error) {
+    console.error('Error fetching countries with pagination', error)
+    throw error
+  }
 }
 
-export async function fetchCountriesByName({
-  params
-}: {
+export async function fetchCountriesByName(
   params: FetchCountriesByNameParams
-}) {
+): Promise<{ data: Country[] }> {
   const searchTerm = params.searchTerm
   const page = params.page || 1
   const pageSize = params.pageSize
-  const response = await fetch(
-    `${baseUrl}name/${searchTerm}?limit=${pageSize}&offset=${
-      (page - 1) * pageSize
-    }${query}`
-  )
-  const data = (await response.json()) as Country[]
-  return { data }
+  try {
+    const response = await fetch(
+      `${baseUrl}name/${searchTerm}?limit=${pageSize}&offset=${
+        (page - 1) * pageSize
+      }${query}`
+    )
+    const data = (await response.json()) as Country[]
+    return { data }
+  } catch (error) {
+    console.error('Error fetching countries by name', error)
+    throw error
+  }
 }
 
 async function fetchCountryNameByCode(countryCode: string): Promise<string> {
-  const response = await fetch(`${baseUrl}alpha/${countryCode}?fields=name`)
-  const country = (await response.json()) as { name: CountryName }
-  return country.name.common
+  try {
+    const response = await fetch(`${baseUrl}alpha/${countryCode}?fields=name`)
+    const country = (await response.json()) as { name: CountryName }
+    return country.name.common
+  } catch (error) {
+    console.error('Error fetching country name by code', error)
+    throw error
+  }
 }
 
 export async function fetchCountry(
   params: FetchCountryParams
 ): Promise<Country> {
   const { countryId } = params
-  const response = await fetch(`${baseUrl}alpha/${countryId}${query}`)
-  let country = (await response.json()) as Country
+  try {
+    const response = await fetch(`${baseUrl}alpha/${countryId}${query}`)
+    let country = (await response.json()) as Country
 
-  if (country.borders) {
-    try {
+    if (country.borders) {
       const borderNames = await Promise.all(
         country.borders.map(fetchCountryNameByCode)
       )
       country.borderCountries = borderNames
-    } catch (error) {
-      console.error('Error fetching border countries', error)
-      throw error
     }
-  }
 
-  return country
+    return country
+  } catch (error) {
+    console.error('Error fetching country', error)
+    throw error
+  }
 }
 
-export async function fetchByRegion({
-  params
-}: {
+export async function fetchByRegion(
   params: FetchByRegionParams
-}) {
+): Promise<{ data: Country[] }> {
   const region = params.region
-  const response = await fetch(`${baseUrl}region/${region}${query}`)
-  const data = (await response.json()) as Country[]
-  return { data }
+  try {
+    const response = await fetch(`${baseUrl}region/${region}${query}`)
+    const data = (await response.json()) as Country[]
+    return { data }
+  } catch (error) {
+    console.error('Error fetching countries by region', error)
+    throw error
+  }
 }
 
 export default {
